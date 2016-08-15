268f2511867c68152e007498fc9eab3e450b692c
freeman <the.freeman.lab@gmail.com>
2016-08-15 14:38:13 -0400

    update demo

--- demo.js
+++ demo.js
@@ -1,11 +1,10 @@
+var util = require('util')
 var grid = require('./index')
 var parse = require('parse-color')
 var position = require('mouse-position')
 
-document.body.style.background = 'black'
-
-var rows = Math.floor(window.innerHeight / 16) - 2
-var columns = Math.floor(window.innerWidth / 16) - 1
+var rows = Math.floor(window.innerHeight * 0.97 / 16) - 2
+var columns = Math.floor(window.innerWidth * 0.97 / 16) - 1
 
 var data = []
 
@@ -21,8 +20,12 @@ var pixels = grid(data, {
   columns: columns, 
   size: 15, 
   padding: 1,
-  background: [0, 0, 0]
+  background: [0, 0, 0],
+  formatted: true
 })
+
+pixels.canvas.style.marginLeft = (window.innerWidth * 0.03) / 2 + 'px'
+pixels.canvas.style.marginTop = (window.innerHeight * 0.04) / 2 + 'px'
 
 var mouse = position(pixels.canvas)
 
@@ -35,17 +38,31 @@ mouse.on('move', function () {
   if (row < rows && column < columns) {
     hue = (hue + 1) % 360
     color = parse('hsl(' + hue + ',50, 50)').rgb
+    document.body.style.background = util.format('rgb(%s,%s,%s)', color[0], color[1], color[2])
     color = color.map(function (d) {return d / 50})
-    data[row * columns + column] = color
-    data[(row - 1) * columns + column] = color
-    data[Math.min((row + 1) * columns + column, data.length)] = color
-    data[row * columns + (column - 1)] = color
-    data[Math.min(row * columns + (column + 1), data.length)] = color
-    data[(row + 1) * columns - column] = color
-    data[(row + 1) * columns - (column + 1)] = color
-    data[row * columns - (column)] = color
-    data[(row + 1) * columns - (column - 1)] = color
-    data[Math.min((row + 2) * columns - (column), data.length)] = color
+
+    for (var i = -1; i < 2; i++) {
+      for (var j = -1; j < 2; j++) {
+        data[Math.min((row + i) * columns + (column + j), data.length)] = color
+        data[Math.min((row + i + 1) * columns - (column + j), data.length)] = color
+      }
+    }
+    
+    // data[row * columns + column] = color
+    // data[(row - 1) * columns + column] = color
+    // data[(row - 1) * columns + (column - 1)] = color
+    // data[Math.min((row + 1) * columns + column, data.length)] = color
+    // data[row * columns + (column - 1)] = color
+    // data[(row - 1) * columns + (column - 1)] = color
+    // data[(row + 1) * columns + (column + 1)] = color
+    // data[Math.min(row * columns + (column + 1), data.length)] = color
+
+    // data[(row + 1) * columns - column] = color
+    // data[(row + 1) * columns - (column + 1)] = color
+    // data[row * columns - (column)] = color
+    // data[(row + 1) * columns - (column - 1)] = color
+    // data[Math.min((row + 2) * columns - (column), data.length)] = color
+
   }
 })
 
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+var tape = require('tape')
+
 // parse should always generate a list of rgb colors
 
 // flat floats
